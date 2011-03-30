c0d3f7700d8b1a476533df6da2cac85b9ae9ec62
Yuji Shimada <xaicron@cpan.org>
2011-03-30 11:36:12 +0000

    Import of XAICRON/Module-Install-TestTarget-0.18 from CPAN.
    gitpan-cpan-distribution: Module-Install-TestTarget
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         XAICRON/Module-Install-TestTarget-0.18.tar.gz
    gitpan-cpan-author:       XAICRON
    gitpan-cpan-maturity:     released

--- lib/Module/Install/TestTarget.pm
+++ lib/Module/Install/TestTarget.pm
@@ -2,7 +2,7 @@ package Module::Install::TestTarget;
 use 5.006_002;
 use strict;
 #use warnings; # XXX: warnings.pm produces a lot of 'redefine' warnings!
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 use base qw(Module::Install::Base);
 use Config;
@@ -107,7 +107,7 @@ sub _quote {
     $code =~ s/\n/ /g;
     if ($^O eq 'MSWin32') {
         $code =~ s/\\\$\$/\$\$/g;
-        if ($Config{make} =~ /dmake/) {
+        if ($Config{make} =~ /dmake/i) {
             $code =~ s/{/{{/g;
             $code =~ s/}/}}/g;
         }
